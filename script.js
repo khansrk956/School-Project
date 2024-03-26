8b750897916ae03bef94d94d7f77f291574836bf
Sharukh khan <khansrk956@gmail.com>
2024-03-26 11:31:11 +0530

    school project

---
+++ script.js
@@ -0,0 +1,18 @@
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("scrollToTopButton").style.display = "block";
+  } else {
+    document.getElementById("scrollToTopButton").style.display = "none";
+  }
+}
+
+document
+  .getElementById("scrollToTopButton")
+  .addEventListener("click", function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  });
