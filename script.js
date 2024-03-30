dd8b89beea59175c16cfa7023e520223baeeb351
Sharukh khan <khansrk956@gmail.com>
2024-03-30 17:08:36 +0530

    Last-before-card complete

--- script.js
+++ script.js
@@ -91,3 +91,14 @@ window.addEventListener("scroll", function () {
   }
 });
 
+
+// card-pop-up javascript
+window.addEventListener("load", function () {
+  setTimeout(
+      function open(event) {
+          document.querySelector(".popup").style.display = "block"
+      }, 1000)
+})
+document.querySelector("#close").addEventListener("click", function () {
+  document.querySelector(".popup").style.display = "none";
+});
