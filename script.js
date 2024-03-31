ce70d41ed5e11550346d05e8f99169c835b34c29
Sharukh khan <khansrk956@gmail.com>
2024-03-31 11:30:23 +0530

    first-page complete

--- script.js
+++ script.js
@@ -102,3 +102,4 @@ window.addEventListener("load", function () {
 document.querySelector("#close").addEventListener("click", function () {
   document.querySelector(".popup").style.display = "none";
 });
+
